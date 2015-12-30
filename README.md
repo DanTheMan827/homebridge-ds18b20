9d4e94a7accf422ad6ea5e989a7b186a41b19e70
Daniel Radtke <daniel.radtke@gmail.com>
2015-12-29 21:02:28 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# homebridge-ds18b20
---
+++ index.js
@@ -0,0 +1,31 @@
+var ds18b20 = require('ds18b20');
+var Service, Characteristic;
+
+module.exports = function(homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  homebridge.registerAccessory("homebridge-temperature-ds18b20", "TemperatureDS18B20", TemperatureAccessory);
+}
+
+function TemperatureAccessory(log, config) {
+  this.log = log;
+  this.name = config["name"];
+  this.device = config["device"];
+
+  this.service = new Service.TemperatureSensor(this.name);
+
+  this.service
+    .getCharacteristic(Characteristic.CurrentTemperature)
+    .on('get', this.getState.bind(this));
+}
+
+TemperatureAccessory.prototype.getState = function(callback) {
+  ds18b20.temperature(this.device, function(err,value){
+    callback(err, value);
+  });
+}
+
+TemperatureFileAccessory.prototype.getServices = function() {
+  return [this.service];
+}
+
