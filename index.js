d912c89586590062e2cb76cc724e7f5342492159
Daniel <790119+DanTheMan827@users.noreply.github.com>
2019-11-27 15:00:59 -0600

    Switch to a newer ds18b20 library (fix #9 hopefully)

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-var ds18b20 = require('ds18b20');
+var ds18b20 = require("ds18b20-raspi");
 var Service, Characteristic;
 
 module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory("homebridge-temperature-ds18b20", "DS18B20", TemperatureAccessory);
-}
+  homebridge.registerAccessory(
+    "homebridge-temperature-ds18b20",
+    "DS18B20",
+    TemperatureAccessory
+  );
+};
 
 function TemperatureAccessory(log, config) {
   this.log = log;
@@ -16,17 +20,13 @@ function TemperatureAccessory(log, config) {
 
   this.service
     .getCharacteristic(Characteristic.CurrentTemperature)
-    .setProps({ minValue: -55, maxValue: 125 })
-    .on('get', this.getState.bind(this));
+    .setProps({ minValue: -100, maxValue: 125 })
+    .on("get", this.getState.bind(this));
 }
 
 TemperatureAccessory.prototype.getState = function(callback) {
-  ds18b20.temperature(this.device, function(err,value){
-    callback(err, value);
-  });
-}
-
+  ds18b20.readC(this.device, 2, callback);
+};
 TemperatureAccessory.prototype.getServices = function() {
   return [this.service];
-}
-
+};
